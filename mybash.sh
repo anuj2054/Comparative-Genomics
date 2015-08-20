ae643d191529dd42159145fdaa579a6d5cdd36d2
Anuj Guruacharya <anuj2054@gmail.com>
2015-08-20 13:09:08 -0500

    Adding 2nd iteration of the script

--- mybash.sh
+++ mybash.sh
@@ -1,16 +1,23 @@
-i=0
-for genome in /mnt/dnarules/Anuj/CnidarianGenome/*.fasta
-do
-((i++))
-echo $genome
-makeblastdb -in $genome -dbtype 'nucl' -out /mnt/dnarules/Anuj/CnidarianGenome/databases/$i
-echo "database created"
-for gene in /mnt/dnarules/Anuj/CnidarianGene/*.fasta
-tblastx -db /mnt/dnarules/Anuj/CnidarianGenome/databases/$i -query $gene -out /mnt/dnarules/Anuj/results/BlastResults.txt -outfmt 6 -max_target_seqs 10 -num_threads 20 -evalue 0.0001
-#echo "blasted with a gene"
+#for genome in $(basename /mnt/dnarules/Anuj/CnidarianGenome/*.fasta)
+#do
+#echo $genome
+#makeblastdb -in /mnt/dnarules/Anuj/CnidarianGenome/$genome -dbtype 'nucl' -out /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome
+#echo "database created"
 #done
-#SubjectSeqID=$(awk '{print $2}' /mnt/dnarules/Anuj/results/BlastResults.txt)
-#awk 'BEGIN {RS=">";} /$SubjectSeqID/ { print ">" $0;}' $genome >  blastResult.fasta
-#echo "blast output pasted onto a file"
+### database has been created so no need to create it again
+
+for genePath in /mnt/dnarules/Anuj/CnidarianGene/*.fasta
+do
+gene=`basename $genePath`
+for genomePath in /mnt/dnarules/Anuj/CnidarianGenome/*.fasta
+do
+genome=`basename $genomePath`
+echo "blasting" $genome " with " $gene
+tblastx -db /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome -query /mnt/dnarules/Anuj/CnidarianGene/$gene -out /mnt/dnarules/Anuj/results/BlastResults.txt -outfmt 6 -max_target_seqs 10 -num_threads 20 -evalue 0.0001
+SSeqID=`Rscript /mnt/dnarules/Anuj/getBestHit.R`
+echo "extracting sequence and keeping it in a file"
+perl /mnt/dnarules/Anuj/extractSequence.pl /mnt/dnarules/Anuj/CnidarianGenome/$genome $SSeqID > $gene$genome.txt
+echo "moving onto next"
+
 done
 done
