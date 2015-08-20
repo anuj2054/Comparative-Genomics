ba99207a78c64c40ced2e1d49ebac489270669b0
Anuj Guruacharya <anuj2054@gmail.com>
2015-08-20 13:11:40 -0500

    third iteration

--- mybash.sh
+++ mybash.sh
@@ -1,4 +1,4 @@
-#for genome in $(basename /mnt/dnarules/Anuj/CnidarianGenome/*.fasta)
+﻿#for genome in $(basename /mnt/dnarules/Anuj/CnidarianGenome/*.fasta)
 #do
 #echo $genome
 #makeblastdb -in /mnt/dnarules/Anuj/CnidarianGenome/$genome -dbtype 'nucl' -out /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome
@@ -15,7 +15,7 @@ genome=`basename $genomePath`
 echo "blasting" $genome " with " $gene
 tblastx -db /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome -query /mnt/dnarules/Anuj/CnidarianGene/$gene -out /mnt/dnarules/Anuj/results/BlastResults.txt -outfmt 6 -max_target_seqs 10 -num_threads 20 -evalue 0.0001
 SSeqID=`Rscript /mnt/dnarules/Anuj/getBestHit.R`
-echo "extracting sequence and keeping it in a file"
+echo "extracting sequence" $SSeqID  "and keeping it in a file"
 perl /mnt/dnarules/Anuj/extractSequence.pl /mnt/dnarules/Anuj/CnidarianGenome/$genome $SSeqID > $gene$genome.txt
 echo "moving onto next"
 
