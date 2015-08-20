235692ac3331b461bafbf690f11b6c7d3b12f00f
Anuj Guruacharya <anuj2054@gmail.com>
2015-08-20 13:13:36 -0500

    fourth iteration

--- mybash.sh
+++ mybash.sh
@@ -1,10 +1,13 @@
-﻿#for genome in $(basename /mnt/dnarules/Anuj/CnidarianGenome/*.fasta)
+﻿
+#for genomePath in /mnt/dnarules/Anuj/CnidarianGenome/*.fasta
 #do
-#echo $genome
-#makeblastdb -in /mnt/dnarules/Anuj/CnidarianGenome/$genome -dbtype 'nucl' -out /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome
+#genome=`basename $genomePath`
+#echo "making database for" $genome
+#makeblastdb -in /mnt/dnarules/Anuj/CnidarianGenome/$genome -dbtype 'nucl' -out /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome -parse_seqids
 #echo "database created"
 #done
 ### database has been created so no need to create it again
+#  awk '/^>/{s=++d".fasta"} {print > s}' uniprot_cnidaria_muscle_pathway.fasta
 
 for genePath in /mnt/dnarules/Anuj/CnidarianGene/*.fasta
 do
@@ -16,7 +19,7 @@ echo "blasting" $genome " with " $gene
 tblastx -db /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome -query /mnt/dnarules/Anuj/CnidarianGene/$gene -out /mnt/dnarules/Anuj/results/BlastResults.txt -outfmt 6 -max_target_seqs 10 -num_threads 20 -evalue 0.0001
 SSeqID=`Rscript /mnt/dnarules/Anuj/getBestHit.R`
 echo "extracting sequence" $SSeqID  "and keeping it in a file"
-perl /mnt/dnarules/Anuj/extractSequence.pl /mnt/dnarules/Anuj/CnidarianGenome/$genome $SSeqID > $gene$genome.txt
+blastdbcmd -db /mnt/dnarules/Anuj/CnidarianGenome/databases/$genome -dbtype nucl -entry $SSeqID -outfmt %f -out /mnt/dnarules/Anuj/results/$gene$genome
 echo "moving onto next"
 
 done
