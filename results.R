59ae5234de7cc6acf0b757c3ca6ba03e00d686dc
Anuj Guruacharya <anuj2054@gmail.com>
2015-11-03 15:13:33 -0600

    the nerve and muscle graphs were ordered

--- results.R
+++ results.R
@@ -5,13 +5,14 @@ RESULTS2_onlyCorrelationCast <- dcast(RESULTS2_onlyCorrelation,p1~p2)
 rownames(RESULTS2_onlyCorrelationCast) <- RESULTS2_onlyCorrelationCast$p1
 RESULTS2_onlyCorrelationCast$p1 <- NULL
 RESULTS2_onlyCorrelationCastMatrix <- data.matrix(RESULTS2_onlyCorrelationCast)
+pdf('muscle.pdf')
+heatmap(RESULTS2_onlyCorrelationCastMatrix[,1:34],Rowv=NA,Colv=NA,col = cm.colors(256))
+dev.off()
 pdf('nerve.pdf')
 heatmap(RESULTS2_onlyCorrelationCastMatrix[,35:223],Rowv=NA,Colv=NA,col = cm.colors(256))
 dev.off()
 pdf('neurotoxin.pdf')
 heatmap(RESULTS2_onlyCorrelationCastMatrix[,224:239],Rowv=NA,Colv=NA,col = cm.colors(256))
 dev.off()
-pdf('muscle.pdf')
-heatmap(RESULTS2_onlyCorrelationCastMatrix[,1:34],Rowv=NA,Colv=NA,col = cm.colors(256))
-dev.off()
+
 
