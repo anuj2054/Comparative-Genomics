cf58d7625d8706b0fce35318aa2a76266c70edc8
Anuj Guruacharya <anuj2054@gmail.com>
2015-11-03 14:22:48 -0600

    added the pdfs for the plots

--- results.R
+++ results.R
@@ -5,4 +5,13 @@ RESULTS2_onlyCorrelationCast <- dcast(RESULTS2_onlyCorrelation,p1~p2)
 rownames(RESULTS2_onlyCorrelationCast) <- RESULTS2_onlyCorrelationCast$p1
 RESULTS2_onlyCorrelationCast$p1 <- NULL
 RESULTS2_onlyCorrelationCastMatrix <- data.matrix(RESULTS2_onlyCorrelationCast)
-heatmap(RESULTS2_onlyCorrelationCastMatrix[,220:239],Rowv=NA,Colv=NA,col = cm.colors(256))
+pdf('nerve.pdf')
+heatmap(RESULTS2_onlyCorrelationCastMatrix[,35:223],Rowv=NA,Colv=NA,col = cm.colors(256))
+dev.off()
+pdf('neurotoxin.pdf')
+heatmap(RESULTS2_onlyCorrelationCastMatrix[,224:239],Rowv=NA,Colv=NA,col = cm.colors(256))
+dev.off()
+pdf('muscle.pdf')
+heatmap(RESULTS2_onlyCorrelationCastMatrix[,1:34],Rowv=NA,Colv=NA,col = cm.colors(256))
+dev.off()
+
