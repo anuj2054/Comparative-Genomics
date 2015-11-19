3a2278e0c508983e40fafb01426f61ab6b1783f3
Anuj Guruacharya <anuj2054@gmail.com>
2015-11-19 16:29:20 -0600

    extracted only the rows that are less than 0.5

--- results.R
+++ results.R
@@ -1,15 +1,14 @@
 library(reshape2)
 setwd("/home/dnarules/Desktop/")
-
 RESULTS<-read.table('results.tsv',header=TRUE,sep='\t')
-
+RESULTS <- RESULTS[RESULTS$p.value<0.05,]
 RESULTS_onlyCorrelation <- data.frame(RESULTS$p1,RESULTS$p2,RESULTS$r)
 RESULTS_onlyCorrelationCast <- dcast(RESULTS_onlyCorrelation,RESULTS.p1~RESULTS.p2)
 rownames(RESULTS_onlyCorrelationCast) <- RESULTS_onlyCorrelationCast$RESULTS.p1
 RESULTS_onlyCorrelationCast$RESULTS.p1 <- NULL
 RESULTS_onlyCorrelationCastMatrix <- data.matrix(RESULTS_onlyCorrelationCast)
 pdf('all.pdf')
-heatmap(RESULTS_onlyCorrelationCastMatrix)
+heatmap(RESULTS_onlyCorrelationCastMatrix,Rowv=NA,Colv=NA,col = cm.colors(256))
 dev.off()
 pdf('muscle.pdf')
 heatmap(RESULTS_onlyCorrelationCastMatrix[,1:34],Rowv=NA,Colv=NA,col = cm.colors(256))
